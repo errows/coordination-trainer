d248566ced400e728d0349216fb87aaa5e50682f
Marc-Antoine Ross <marc@proze.net>
2020-01-11 10:20:57 -0500

    Initial Commit

---
+++ CoordinationTrainer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoordinationTrainer
+//
+//  Created by Marc-Antoine Ross on 2020-01-11.
+//  Copyright © 2020 Marc-Antoine Ross. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
